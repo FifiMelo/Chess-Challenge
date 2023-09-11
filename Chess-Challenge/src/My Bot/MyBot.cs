67c1a1066c70ac4da442ecdcd615e5f29404a2c4
FifiMelo <felipe336336@gmail.com>
2023-09-11 14:38:34 +0200

    material_eval

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,136 +6,37 @@ using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
-    public Move the_best_move;
-    public int fixed_depth = 5;
-    public int[,] white_pawn_movement = new int[2, 2]
-{
-        { 7, 1},
-        { 9, 1},
-};
-    public int[,] black_pawn_movement = new int[2, 2]
+    public
+    Move the_best_move;
+    int fixed_depth = 5;
+    int[] material = new int[7] { 0, 1, 3, 3, 5, 9, 0 };
+
+
+    float StaticEvaluation(Board board)
     {
-        { -7, -1},
-        { -9, -1},
-    };
-    public int[,] knight_movement = new int[8, 2]
-    {
-        {15, 2},
-        {17, 2},
-        {10, 1},
-        {-6, -1},
-        {-15, -2},
-        {-17, -2},
-        {-10, -1},
-        {6, 1},
-    };
-    public int[,] king_movement = new int[8, 2]
-    {
-        {7 , 1},
-        {8 , 1},
-        {9 , 1},
-        {1 , 0},
-        {-7 , -1},
-        {-8 , -1},
-        {-9 , -1},
-        {-1 , 0},
-    };
-
-
-
-    public float StaticEvaluation(Board board)
-    {
-        int[] control = new int[64];
-        int eval = 0;
-
-
-
-        foreach (PieceList piecelist in board.GetAllPieceLists())
+        Move[] moves = board.GetLegalMoves();
+        foreach(Move move in moves)
         {
-            foreach (Piece piece in piecelist)
-            {
 
-                int square = piece.Square.Index;
-                if (piece.IsKnight)
-                {
-
-                    for (int i = 0; i < 8; i++)
-                    {
-                        Console.WriteLine(square / 8 + knight_movement[i, 1]);
-                        Console.WriteLine((square + knight_movement[i, 0]) / 8);
-                        if (square / 8 + knight_movement[i, 1] == (square + knight_movement[i, 0]) / 8 &&
-                            0 <= square + knight_movement[i, 0] &&
-                            square + knight_movement[i, 0] < 64)
-                        {
-                            control[square + knight_movement[i, 0]] += piece.IsWhite ? 1 : -1;
-                            
-                        }
-                        
-                    }
-                    eval += piece.IsWhite ? 3 : -3;
-                }
-
-                if (piece.IsPawn && piece.IsWhite)
-                {
-                    for (int i = 0; i < 2; i++)
-                    {
-                        if (square / 8 + white_pawn_movement[i, 1] == (square + white_pawn_movement[i, 0]) / 8)
-                        {
-                            control[square + white_pawn_movement[i, 0]]++;
-                            
-                        }
-                    }
-                    eval++;
-                }
-
-                if (piece.IsPawn && !piece.IsWhite)
-                {
-                    for (int i = 0; i < 2; i++)
-                    {
-                        if (square / 8 + black_pawn_movement[i, 1] == (square + black_pawn_movement[i, 0]) / 8)
-                        {
-                            control[square + black_pawn_movement[i, 0]]--;
-                            
-                        }
-                    }
-                    eval--;
-                }
-
-                if (piece.IsKing)
-                {
-                    for (int i = 0; i < 8; i++)
-                    {
-                        if (square / 8 + king_movement[i, 1] == (square + king_movement[i, 0]) / 8 &&
-                            0 <= square + king_movement[i, 0] &&
-                            square + king_movement[i, 0] < 64)
-                        {
-                            control[square + king_movement[i, 0]] += piece.IsWhite ? 1 : -1;
-                        }
-                    }
-                }
-
-            }
         }
+        return 2;
+    }
 
+    float material_eval(Board board, bool all_positive = false)
+    {
 
+        float e = 0;
+        foreach(PieceList list in board.GetAllPieceLists())
+        {
+            foreach(Piece piece in list)
+            {
+                e += material[(int)piece.PieceType] * (piece.IsWhite ? 1 : all_positive ? 1 : -1);
+            }
 
-
-        int e = 0;
-        e += board.GetPieceList(PieceType.Rook, true).Count * 5;
-        e += board.GetPieceList(PieceType.Queen, true).Count * 9;
-        e += board.GetPieceList(PieceType.Bishop, true).Count * 3;
-        e += board.GetPieceList(PieceType.Knight, true).Count * 3;
-        e += board.GetPieceList(PieceType.Pawn, true).Count * 1;
-
-        e += board.GetPieceList(PieceType.Rook, false).Count * -5;
-        e += board.GetPieceList(PieceType.Queen, false).Count * -9;
-        e += board.GetPieceList(PieceType.Bishop, false).Count * -3;
-        e += board.GetPieceList(PieceType.Knight, false).Count * -3;
-        e += board.GetPieceList(PieceType.Pawn, false).Count * -1;
-
+        }
         return e;
     }
-    public int compare_moves(Move move1, Move move2)
+    int compare_moves(Move move1, Move move2)
     {
         int result = 0;
         if (move1.IsCapture)
@@ -155,7 +56,7 @@ public class MyBot : IChessBot
         return result > 0 ? 1 : (result < 0 ? -1 : 0);
     }
 
-    public float Minimax(Board board, int depth, float alpha, float beta)
+    float Minimax(Board board, int depth, float alpha, float beta)
     {
 
 
